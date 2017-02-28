b733e4f4c4f3c04faed780263e126e02ce68807d
cwicoken <kmueller@cwico.com>
2017-02-28 11:30:03 -0500

    Initial Commit

---
+++ orahash/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  orahash
+//
+//  Created by Ken Mueller on 2/28/17.
+//  Copyright © 2017 Ken Mueller. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
